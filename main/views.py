42671b24257a320dab2ad2d14f8fce4665464c5d
Olga-chum <olga.chumakova2002@mail.ru>
2024-04-23 17:44:15 +0400

    the main app is ready, pages index, about, sales and else

--- main/views.py
+++ main/views.py
@@ -4,11 +4,25 @@ from django.shortcuts import render
 
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'blablabla'
+        'title': 'Chapter & Verse - Главная'        
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict = {
+        'title': 'Chapter & Verse - О нас'
+    }
+    return render(request, 'main/about.html', context)
+
+def sales(request):
+    context: dict = {
+        'title': 'Chapter & Verse - Акции'
+    }
+    return render(request, 'main/sales.html', context)
+
+def else_(request):
+    context: dict = {
+        'title': 'Chapter & Verse - Что еще почитать?'
+    }
+    return render(request, 'main/else.html', context)
