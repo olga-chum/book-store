3b99662b2c4450e0157316cc96c03bddaa28f74b
Olga-chum <olga.chumakova2002@mail.ru>
2024-04-24 21:37:39 +0400

    снесла все к хуям

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name 
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    author_name = models.CharField(max_length=150, null=True, blank=True, verbose_name='Автор')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.PositiveIntegerField(default=0, max_length=6, verbose_name='Цена')
+    discount = models.PositiveIntegerField(default=0, max_length=6, null=True, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, max_length=6, null=True, verbose_name='Количество')
+    age = models.PositiveIntegerField(default=0, max_length=6, null=True, verbose_name='Возраст')
+    year_of_publish = models.PositiveIntegerField(max_length=4, null=True, verbose_name='Год издания')
+    publisher = models.CharField(max_length=150,null=True, verbose_name='Издательство')
+    cycle = models.CharField(max_length=150, null =True, blank=True, verbose_name='Цикл')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.template import context
 
 def catalog(request):
     return render(request, 'goods/catalog.html')
 
 def classica(request):
-    return render(request, 'goods/classica.html')
+    context = {
+        'title': 'Chepter & Verse: Классика'
+    }
+    return render(request, 'goods/classica.html', context)
 
 def product(request):
     return render(request, 'goods/product.html')
