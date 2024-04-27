6bee70fec049374a706de383949ad95dcf817c48
Olga-chum <olga.chumakova2002@mail.ru>
2024-04-27 12:56:04 +0400

    slug converter for product

--- goods/urls.py
+++ goods/urls.py
@@ -5,7 +5,7 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.classica, name='index'),
-    path('classica/', views.classica, name='classica'),
-    path('product/', views.product, name='product')
+    path('', views.catalog, name='index'),
+    path('product/<int:product_id>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product')
 ]
--- goods/views.py
+++ goods/views.py
@@ -3,7 +3,7 @@ from django.template import context
 
 from goods.models import Products
 
-def classica(request):
+def catalog(request):
 
     goods= Products.objects.all()
 
@@ -11,7 +11,17 @@ def classica(request):
         'title': 'Chepter & Verse: Классика',
         "goods": goods
     }
-    return render(request, 'goods/classica.html', context)
+    return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug=False, product_id=False):
+
+    if product_id:
+        product = Products.objects.get(id=product_id)
+    else:
+        product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'goods/product.html', context=context)
