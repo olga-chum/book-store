9aa6be3468257c8cf13ab562642b81c3c7851b4c
Olga-chum <olga.chumakova2002@mail.ru>
2024-04-30 19:43:38 +0400

    full text search well-done!!!

--- goods/utils.py
+++ goods/utils.py
@@ -7,9 +7,9 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    vector = SearchVector("name", "author_name", "description")
+    vector = SearchVector("name", "author_name")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
     # keywords = [word for word in query.split() if len(word) > 2]
 
     # q_objects = Q()
